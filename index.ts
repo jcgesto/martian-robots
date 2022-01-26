e90cbb71cecebec1fb5b96907968aa9876a1128f
Juan Carlos Gesto García <jc.gesto@gmail.com>
2022-01-26 17:29:00 +0100

    first working version

--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ import { MartianRobotsService } from './src/application/MartianRobotsService';
 import { MartianRobots } from './src/infrastructure/MartianRobots';
 import { TextFileInputReader } from './src/infrastructure/TextFileInputReader';
 import { TextFileOutputWriter } from './src/infrastructure/TextFileOutputWriter';
+import { TextFileInputParser } from './src/infrastructure/TextFileInputParser';
 
 console.log('MARTIAN ROBOTS\n')
 
-const textFileInputReader = new TextFileInputReader('input.txt', fs)
+const textFileInputParser = new TextFileInputParser()
+const textFileInputReader = new TextFileInputReader('input.txt', fs, textFileInputParser)
 const textFileOutputWriter = new TextFileOutputWriter('output.txt', fs)
 const martianRobotsService = new MartianRobotsService()
 const martianRobots = new MartianRobots(textFileInputReader, textFileOutputWriter, martianRobotsService)
